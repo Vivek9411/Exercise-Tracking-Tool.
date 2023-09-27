bacd4b98a71b7e28052257c574bf724fadbe0258
Vivek9411 <146237718+Vivek9411@users.noreply.github.com>
2023-09-27 23:51:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 import smtplib
 
-sender_email = '2100520100163@ietlucknow.ac.in'
-python = "nvaxsuvrjvmurgsf"
+sender_email = 'youemailaddresss'
+# app password to login
+python = "secure_code_to_sendemail"
+# list of receivers mails
+receivers = []
+
+#list of item you want to buy
 
 list_of_url = [
     "https://www.amazon.in/gp/product/B09TTSM8HB/ref=sw_img_1?smid=A14CZOWI0VEHLG&psc=1"
 ]
+# list of corresponding minimum price
 to_buy_price = [7000]
 for i in range(len(list_of_url)):
     item = list_of_url[i]
@@ -20,8 +26,9 @@ for i in range(len(list_of_url)):
     final_price = int(float(final_price.removesuffix(".")))
     print(final_price)
     if final_price < to_buy_price[i]:
-        connection = smtplib.SMTP("smtp.gmail.com")
-        connection.starttls()
-        connection.login(user=sender_email, password=python)
-        connection.sendmail(from_addr=sender_email, to_addrs=sender_email, msg=f"price-alert/n/n/n {item} to buy")
-        connection.close()
+        for receviers_address in receivers:
+            connection = smtplib.SMTP("smtp.gmail.com")
+            connection.starttls()
+            connection.login(user=sender_email, password=python)
+            connection.sendmail(from_addr=sender_email, to_addrs=receviers_address, msg=f"price-alert/n/n/n {item} to buy")
+            connection.close()
