7c38371c53eb98d8509dbd094db08d449c06d159
Vivek9411 <146237718+Vivek9411@users.noreply.github.com>
2023-09-27 23:34:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import requests
+import smtplib
+
+sender_email = '2100520100163@ietlucknow.ac.in'
+python = "nvaxsuvrjvmurgsf"
+
+list_of_url = [
+    "https://www.amazon.in/gp/product/B09TTSM8HB/ref=sw_img_1?smid=A14CZOWI0VEHLG&psc=1"
+]
+to_buy_price = [7000]
+for i in range(len(list_of_url)):
+    item = list_of_url[i]
+    response = requests.get(url=item)
+    soup = BeautifulSoup(response.text, "html.parser")
+    t = soup.find(name="span", class_="a-price-whole")
+    print(t.getText)
+    l = t.get_text()
+    final_price = l.replace(",", "")
+    final_price = int(float(final_price.removesuffix(".")))
+    print(final_price)
+    if final_price < to_buy_price[i]:
+        connection = smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(user=sender_email, password=python)
+        connection.sendmail(from_addr=sender_email, to_addrs=sender_email, msg=f"price-alert/n/n/n {item} to buy")
+        connection.close()
